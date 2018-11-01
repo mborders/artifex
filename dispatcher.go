25e17bf4600a6e899c76c99012df672de60947f4
Matthew Borders <mborders@outlook.com>
2018-10-31 22:38:13 -0500

    Added DispatchIn/DispatchEvery functions for scheduling jobs in the future

--- dispatcher.go
+++ dispatcher.go
@@ -1,6 +1,9 @@
 package queue
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // Dispatcher maintains a pool for available workers
 // and a job queue that workers will process
@@ -39,6 +42,35 @@ func (d *Dispatcher) Dispatch(job Job) {
 	d.JobQueue <- job
 }
 
+// DispatchIn pushes the given job into the job queue
+// after the given duration has elapsed
+func (d *Dispatcher) DispatchIn(job Job, duration time.Duration) {
+	go func() {
+		time.Sleep(duration)
+		d.JobQueue <- job
+	}()
+}
+
+// DispatchEvery pushes the given job into the job queue
+// continuously at the given interval
+func (d *Dispatcher) DispatchEvery(job Job, interval time.Duration) *DispatchTicker {
+	t := time.NewTicker(interval)
+	dt := &DispatchTicker{ticker: t, quit: make(chan bool)}
+
+	go func() {
+		for {
+			select {
+			case <-t.C:
+				d.JobQueue <- job
+			case <-dt.quit:
+				return
+			}
+		}
+	}()
+
+	return dt
+}
+
 func (d *Dispatcher) start() {
 	for {
 		select {
@@ -49,4 +81,18 @@ func (d *Dispatcher) start() {
 			}(job)
 		}
 	}
+}
+
+// DispatchTicker represents a dispatched job ticker
+// that executes on a given interval. This provides
+// a means for stopping the execution cycle from continuing.
+type DispatchTicker struct {
+	ticker *time.Ticker
+	quit   chan bool
+}
+
+// Stop ends the execution cycle for the given ticker.
+func (dt *DispatchTicker) Stop() {
+	dt.ticker.Stop()
+	dt.quit <- true
 }
--- dispatcher_test.go
+++ dispatcher_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func TestQueue(t *testing.T) {
+func TestDispatcher_Dispatch(t *testing.T) {
 	a := 0
 	b := 0
 	c := 0
@@ -27,17 +27,17 @@ func TestQueue(t *testing.T) {
 		c = 3
 	}})
 
-	time.Sleep(time.Second * 3)
+	time.Sleep(time.Second)
 	assert.Equal(t, 1, a)
 	assert.Equal(t, 2, b)
 	assert.Equal(t, 3, c)
 }
 
-func TestQueue_Mutex(t *testing.T) {
+func TestDispatcher_Dispatch_Mutex(t *testing.T) {
 	n := 100
 	mutex := &sync.Mutex{}
 
-	d := NewDispatcher(10, 3)
+	d := NewDispatcher(10, n)
 	d.Run()
 
 	var v []int
@@ -50,6 +50,58 @@ func TestQueue_Mutex(t *testing.T) {
 		}})
 	}
 
-	time.Sleep(time.Second * 3)
+	time.Sleep(time.Second)
 	assert.Equal(t, n, len(v))
+}
+
+func TestDispatcher_DispatchIn(t *testing.T) {
+	v := false
+
+	d := NewDispatcher(1, 1)
+	d.Run()
+
+	d.DispatchIn(Job{Run: func() {
+		v = true
+	}}, time.Millisecond*300)
+
+	time.Sleep(time.Millisecond * 100)
+	assert.False(t, v)
+
+	time.Sleep(time.Millisecond * 100)
+	assert.False(t, v)
+
+	time.Sleep(time.Millisecond * 150)
+	assert.True(t, v)
+}
+
+func TestDispatcher_DispatchEvery(t *testing.T) {
+	c := 0
+
+	d := NewDispatcher(1, 3)
+	d.Run()
+
+	d.DispatchEvery(Job{Run: func() {
+		c++
+	}}, time.Millisecond*300)
+
+	time.Sleep(time.Second)
+	assert.Equal(t, 3, c)
+}
+
+func TestDispatcher_DispatchEvery_Stop(t *testing.T) {
+	c := 0
+
+	d := NewDispatcher(1, 3)
+	d.Run()
+
+	dt := d.DispatchEvery(Job{Run: func() {
+		c++
+	}}, time.Millisecond*100)
+
+	time.Sleep(time.Millisecond * 550)
+	assert.Equal(t, 5, c)
+	dt.Stop()
+
+	time.Sleep(time.Millisecond * 500)
+	assert.Equal(t, 5, c)
 }
